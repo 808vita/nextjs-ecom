c6bd9ca5cc6a3c4fb825ac4aa261ebd8f55f614b
808vita <its808vita@gmail.com>
2022-10-04 22:31:09 +0530

    header | product card - starters

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import ProductCard from '../components/ProductCard'
 
 
 export default function Home() {
   return (
     <div>
      <Header/>
+     <ProductCard/>
+     <ProductCard/>
     </div>
   )
 }
