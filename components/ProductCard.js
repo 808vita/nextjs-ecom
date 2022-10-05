6760cfd55a3091f11409d405c479ebfd26daef2f
808vita <its808vita@gmail.com>
2022-10-05 14:00:49 +0530

    add to cart starter

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,8 +1,35 @@
 import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({ data }) => {
+  const handleClick = () => {
+    try {
+      let cartItems = JSON.parse(localStorage.getItem("cartItems"));
+      console.log(cartItems);
+      if (!cartItems) {
+        localStorage.setItem("cartItems", JSON.stringify([data.id]));
+
+        cartItems = JSON.parse(localStorage.getItem("cartItems"));
+        console.log(cartItems);
+        return;
+      }
+      console.log(cartItems, "oof not null");
+      if (cartItems.includes(data.id)) {
+        console.log(cartItems, "already added to cart");
+
+        return;
+      }
+
+      cartItems.push(data.id);
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+      cartItems = JSON.parse(localStorage.getItem("cartItems"));
+      console.log(cartItems);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="col-sm-4">
+    <div className="col-sm-4 mb-3">
       <div className="card" style={{ width: "18rem" }}>
         <img
           src="https://avatars.githubusercontent.com/u/97225946?v=4"
@@ -10,14 +37,14 @@ const ProductCard = () => {
           alt="..."
         />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
+          <h5 className="card-title">
+            {data.title} ${data.price}
+          </h5>
+
+          <p className="card-text">{data.desc}</p>
+          <button className="btn btn-primary" onClick={() => handleClick()}>
             Add To Cart
-          </a>
+          </button>
         </div>
       </div>
     </div>
