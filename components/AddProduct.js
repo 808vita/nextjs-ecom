3df91f2a7ea4f3a2a42a5c4a0607c37ea640ff14
808vita <its808vita@gmail.com>
2022-10-06 19:33:08 +0530

    add product starter

--- components/AddProduct.js
+++ components/AddProduct.js
@@ -1,7 +1,65 @@
-import React from "react";
+import { useState } from "react";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [price, setPrice] = useState("");
+  const [Description, setDescription] = useState("");
+
+  const addProduct = () => {
+    if (!title || !type || type === "Type" || !price || !Description) {
+      console.log("please fill all fields");
+      return;
+    }
+
+    console.log(title, type, price, Description);
+    console.log("oof");
+  };
+  return (
+    <div>
+      <div className="input-group input-group-sm mb-3">
+        <span className="input-group-text">Title</span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <select
+        className="form-select form-select-sm mb-3"
+        aria-label=".form-select-sm example"
+        onChange={(e) => setType(e.target.value)}
+      >
+        <option>Type</option>
+        <option value="Laptop">Laptop</option>
+        <option value="Mobile">Mobile</option>
+        <option value="Guitar">Guitar</option>
+      </select>
+      <div className="input-group input-group-sm mb-3">
+        <span className="input-group-text">Price</span>
+        <input
+          type="number"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </div>
+      <div className="input-group input-group-sm mb-3">
+        <span className="input-group-text">Description</span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <button onClick={() => addProduct()}>Add Product</button>
+    </div>
+  );
 };
 
 export default AddProduct;
--- pages/admin.js
+++ pages/admin.js
@@ -7,7 +7,8 @@ export default function Admin() {
       <h1>admin page</h1>
       <h2>edit deatais</h2>
       <h2>add product</h2>
-      <EditProduct />
+      {/* <EditProduct /> */}
+      <AddProduct />
     </div>
   );
 }
