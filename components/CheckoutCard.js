b662151f16351f44739f4850f6bda41f7680b4ac
808vita <its808vita@gmail.com>
2022-10-07 04:36:52 +0530

    icons - starter

--- components/CheckoutCard.js
+++ components/CheckoutCard.js
@@ -23,16 +23,16 @@ const CheckoutCard = ({ data }) => {
             <h3 className="card-title">{data.title}</h3>
             <h5 className="card-title">${data.price}</h5>
           </div>
-          <span className="btn btn-outline-success ">
+          <span className="btn btn-outline-success pt-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-check "
+              className="bi bi-check-square-fill"
               viewBox="0 0 16 16"
             >
-              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
             </svg>
           </span>
         </div>
