6496c475e0f969383f52f4c894e70023be085a89
808vita <its808vita@gmail.com>
2022-10-07 07:19:07 +0530

    header

--- components/Header.js
+++ components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
           </a>
         </Link>
 
-        <div className="col-md-3 text-end me-5 mx-5">
+        <div className="col-md-4 text-end me-5 mx-5">
           <div className="d-flex flex-wrap align-items-center justify-content-center">
             <Link href="/cart">
               <a type="button" className="btn btn-outline-success m-3">
@@ -43,9 +43,26 @@ const Header = () => {
               <a
                 href="/api/auth/login"
                 type="button"
-                className="btn btn-primary me-2"
+                className="btn btn-primary m-2"
               >
                 Login
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-box-arrow-in-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
               </a>
             ) : (
               <div className="flex-shrink-0 dropdown m-3">
