954c147cfee289adea86453550757da75cf94e83
808vita <its808vita@gmail.com>
2022-10-07 21:43:16 +0530

    orderscard - layout & additions

--- components/OrdersCard.js
+++ components/OrdersCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CheckoutCard from "./CheckoutCard";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const OrdersCard = ({ order, index }) => {
   //   console.log(order, index);
@@ -9,8 +10,20 @@ const OrdersCard = ({ order, index }) => {
         style={{ "--bs-border-opacity": 0.3 }}
         className="p-5 mb-5 border border-secondary rounded"
       >
-        <h5>Latest Order</h5>
-        <p key={order._id}>order id: {order._id}</p>
+        <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
+          <h4 className="text-muted pb-2">Latest Order</h4>
+          <div className="d-flex flex-wrap flex-column align-items-start justify-content-center">
+            <p key={order._id} className="text-muted">
+              order id: {order._id}
+            </p>
+            <p key={order.createdAt} className="text-muted">
+              Placed Order:{" "}
+              {formatDistanceToNow(new Date(order.createdAt), {
+                addSuffix: true,
+              })}
+            </p>
+          </div>
+        </div>
 
         {order?.orderedItems.map((item) => (
           <CheckoutCard key={item._id} data={item} />
@@ -23,7 +36,19 @@ const OrdersCard = ({ order, index }) => {
         style={{ maxWidth: "800px", "--bs-border-opacity": 0.3 }}
         className="p-5 mb-5 border border-secondary rounded"
       >
-        <p key={order._id}>order id: {order._id}</p>
+        <div className="d-flex flex-wrap flex-column align-items-center justify-content-center">
+          <div className="d-flex flex-wrap flex-column align-items-start justify-content-center">
+            <p key={order._id} className="text-muted">
+              order id: {order._id}
+            </p>
+            <p key={order.createdAt} className="text-muted">
+              Placed Order:{" "}
+              {formatDistanceToNow(new Date(order.createdAt), {
+                addSuffix: true,
+              })}
+            </p>
+          </div>
+        </div>
         {order?.orderedItems.map((item) => (
           <CheckoutCard key={item._id} data={item} />
         ))}
